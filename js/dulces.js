d2a66db6198b981f816e3aa0a8ce91ee6a16a829
Ricardo Ramos <75505109+disenowebr@users.noreply.github.com>
2025-07-30 15:06:23 -0600

    Add files via upload
    Arreglo del menu para dispositivos

--- js/dulces.js
+++ js/dulces.js
@@ -2,10 +2,31 @@ const menu = document.querySelector("#menu");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
-abrir.addEventListener("click", () => {
-    menu.classList.add("visible");
-} )
 
-cerrar.addEventListener("click", () =>{
-    menu.classList.remove("visible");
+function abrirMenu() {
+    menu.classList.toggle("visible");
+}
+
+abrir.addEventListener("click", abrirMenu);
+cerrar.addEventListener("click", abrirMenu);
+
+const menuLinksClose = document.querySelectorAll("#menu a[href^='#']");
+menuLinksClose.forEach(menuLinksClose => {
+    menuLinksClose.addEventListener("click", function () {
+        menu.classList.remove("visible");
+    })
 })
+
+
+
+
+
+
+// abrir.addEventListener("click", () => {
+//     menu.classList.add("visible");
+// } )
+
+// cerrar.addEventListener("click", () =>{
+//     menu.classList.remove("visible");
+// })
+
